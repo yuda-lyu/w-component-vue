14af42af6d248e176e26bf8bf45d0a1eee30ea99
yuda-lyu <firsemisphere@gmail.com>
2019-10-01 21:52:28 +0800

    modify: add exams for WJsonView and WDynamicScroll

--- src/AppZoneWDynamicScroll.vue
+++ src/AppZoneWDynamicScroll.vue
@@ -8,7 +8,37 @@
         <div style="padding-left:30px;">
 
 
-            <div class="bk" style="display:block;">
+            <div class="bk">
+                <demolink
+                    :kbname="'w-dynamic-scroll'"
+                    :casename="'default'"
+                    :kind="'nokind'"
+                    :shell="'pure'"
+                ></demolink>
+
+                <w-dynamic-scroll
+                    style="border:1px solid #ddd; width:400px;"
+                    :rows="WDynamicScroll.data1"
+                >
+                    <template v-slot:block="props">
+
+                        <div style="display:table; padding:3px 10px;">
+
+                            <div style="display:table-cell; color:#f26;">{{props.irow}}</div>
+
+                            <div style="display:table-cell; padding-right:10px;">:</div>
+
+                            <div style="display:table-cell; color:#999;">{{props.row}}</div>
+
+                        </div>
+
+                    </template>
+                </w-dynamic-scroll>
+
+            </div>
+
+
+            <div class="bk">
                 <demolink
                     :kbname="'w-dynamic-scroll'"
                     :casename="'listHeight'"
@@ -17,13 +47,43 @@
                 ></demolink>
 
                 <w-dynamic-scroll
-                    style="border:1px solid #ddd;"
-                    :listHeight="500"
-                    :rows="WDynamicScroll.data"
+                    style="border:1px solid #ddd; width:400px;"
+                    :listHeight="250"
+                    :rows="WDynamicScroll.data1"
                 >
                     <template v-slot:block="props">
 
-                        <div style="display:table; padding:0px 10px;">
+                        <div style="display:table; padding:3px 10px;">
+
+                            <div style="display:table-cell; color:#f26;">{{props.irow}}</div>
+
+                            <div style="display:table-cell; padding-right:10px;">:</div>
+
+                            <div style="display:table-cell; color:#999;">{{props.row}}</div>
+
+                        </div>
+
+                    </template>
+                </w-dynamic-scroll>
+
+            </div>
+
+
+            <div class="bk" style="display:block;">
+                <demolink
+                    :kbname="'w-dynamic-scroll'"
+                    :casename="'large data (100000 lines)'"
+                    :kind="'nokind'"
+                    :shell="'pure'"
+                ></demolink>
+
+                <w-dynamic-scroll
+                    style="border:1px solid #ddd;"
+                    :rows="WDynamicScroll.data2"
+                >
+                    <template v-slot:block="props">
+
+                        <div style="display:table; padding:3px 10px;">
 
                             <div style="display:table-cell; color:#f26;">{{props.irow}}</div>
 
@@ -78,7 +138,19 @@ export default {
     data: function() {
         return {
             'WDynamicScroll': {
-                'data': (function(a) {
+                'data1': [
+                    'Examine trends in the movement of guns across different states.',
+                    'Code of Federal Regulations - Alcohol, Tobacco Products and Firearms',
+                    '1980-2014 November General Election Turnout and Registration',
+                    'Reported Voting and Registration, by Sex and Single Years of Age: November 2016',
+                    'felon-disenfranchisement',
+                    '623e708e-574e-45a2-9dc9-b53178903325',
+                    'https://assembly-public-spotlight.s3.amazonaws.com/assets/images/public-welcome/card_disenfranchisement.jpg',
+                    'Map felon disenfranchisement patterns by state.',
+                    'Estimate how felon enfranchisement could affect close elections. Track how disenfranchisement has varied over time. an American non-profit journalism outlet dedicated to expanding media coverage on the state of guns in the U.S., spent 11 months amassing data on stolen and recovered guns. They filed hundreds of public record requests to over 1,000 law enforcement agencies in 36 states to investigate what happens to guns when they are reported stolen. The data reveals that guns were recovered by a variety of means, from being turned in as evidence or by abandonment.',
+                    'Vivien Ngo, Data Journalism Fellow.',
+                ],
+                'data2': (function(a) {
                     a /= 5; let b = ['Nam ac nibh imperdiet, gravida metus eget, sodales eros.', 'Duis accumsan urna eu risus congue efficitur.', 'Etiam eget dolor sit amet ipsum porttitor lacinia vel at diam.', 'Sed pulvinar leo ac elit ultricies scelerisque.', 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.']; let c = []; for (let d = 0; d < a; d++) for (let a = 0; a < b.length; a++)c.push(b[a]); return c
                 })(100001), //1000001 100001 1000
             },
--- src/AppZoneWJsonView.vue
+++ src/AppZoneWJsonView.vue
@@ -8,7 +8,23 @@
         <div style="padding-left:30px;">
 
 
-            <div class="bk" style="display:block;">
+            <div class="bk">
+                <demolink
+                    :kbname="'w-json-view'"
+                    :casename="'default'"
+                    :kind="'nokind'"
+                    :shell="'pure'"
+                ></demolink>
+
+                <w-json-view
+                    style="border:1px solid #ddd; width:400px;"
+                    :data="WJsonView.data1"
+                ></w-json-view>
+
+            </div>
+
+
+            <div class="bk">
                 <demolink
                     :kbname="'w-json-view'"
                     :casename="'listHeight'"
@@ -17,9 +33,48 @@
                 ></demolink>
 
                 <w-json-view
+                    style="border:1px solid #ddd; width:400px;"
+                    :listHeight="250"
+                    :data="WJsonView.data1"
+                ></w-json-view>
+
+            </div>
+
+
+            <div class="bk">
+                <demolink
+                    :kbname="'w-json-view'"
+                    :casename="'iconColor & keyColor & keyNumbersColor & numColor & strColor & bolColor & funColor'"
+                    :kind="'nokind'"
+                    :shell="'pure'"
+                ></demolink>
+
+                <w-json-view
+                    style="border:1px solid #ddd; width:400px;"
+                    :iconColor="'#aaa'"
+                    :keyColor="'#f6a'"
+                    :keyNumbersColor="'#f26'"
+                    :numColor="'#00f'"
+                    :strColor="'#bbb'"
+                    :bolColor="'#c5f'"
+                    :funColor="'#fa6'"
+                    :data="WJsonView.data1"
+                ></w-json-view>
+
+            </div>
+
+
+            <div class="bk" style="display:block;">
+                <demolink
+                    :kbname="'w-json-view'"
+                    :casename="'large data (20000 lines)'"
+                    :kind="'nokind'"
+                    :shell="'pure'"
+                ></demolink>
+
+                <w-json-view
                     style="border:1px solid #ddd;"
-                    :listHeight="500"
-                    :data="WJsonView.data"
+                    :data="WJsonView.data2"
                 ></w-json-view>
 
             </div>
@@ -117,7 +172,8 @@ export default {
     data: function() {
         return {
             'WJsonView': {
-                'data': (function(a) {
+                'data1': { 'squadName': 'Super hero squad', 'fun': function() {}, 'homeTown': 'Metro City', 'formed': 2016, 'secretBase': 'Super tower', 'active': true, 'members': [{ 'name': 'Molecule Man', 'age': 29, 'secretIdentity': 'Dan Jukes', 'powers': ['Radiation resistance', 'Turning tiny', 'Radiation blast'] }, { 'name': 'Madame Uppercut', 'age': 39, 'secretIdentity': 'Jane Wilson', 'powers': ['Million tonne punch', 'Damage resistance', 'Superhuman reflexes'] }, { 'name': 'Eternal Flame', 'age': 1000000, 'secretIdentity': 'Unknown', 'powers': ['Immortality', 'Heat Immunity', 'Inferno', 'Teleportation', 'Interdimensional travel'] }] },
+                'data2': (function(a) {
                     a /= 200; a = Math.max(a, 1); let b = { ph1_a: 123, ph1_b: 45.67, ph1_c: '123.456', ph1_d: 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', ph1_e: !0, ph1_g: function() {}, ph1_h: [], ph1_j: {}, ph1_k: { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }, ph1_l: [123, 45.67, '123.456', 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', !0, !1, function() {}, { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }], ph1_z: { ph2_a: 123, ph2_b: 45.67, ph2_c: '123.456', ph2_d: 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', ph2_e: !0, ph2_g: function() {}, ph2_h: [], ph2_j: {}, ph2_k: { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }, ph2_l: [123, 45.67, '123.456', 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', !0, !1, function() {}, { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }], ph2_z: { ph3_a: 123, ph3_b: 45.67, ph3_c: '123.456', ph3_d: 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', ph3_e: !0, ph3_g: function() {}, ph3_h: [], ph3_j: {}, ph3_k: { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }, ph3_l: [123, 45.67, '123.456', 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', !0, !1, function() {}, { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }], ph3_z: { ph4_a: 123, ph4_b: 45.67, ph4_c: '123.456', ph4_d: 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', ph4_e: !0, ph4_g: function() {}, ph4_h: [], ph4_j: {}, ph4_k: { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }, ph4_l: [123, 45.67, '123.456', 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', !0, !1, function() {}, { a: 321, b: 76.54, c: '654.321', d: '321 zyx', e: !0, f: [], g: {} }], ph4_z: { ph5_a: 123, ph5_b: 45.67, ph5_c: '123.456', ph5_d: 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', ph5_e: !0, ph5_f: !1, ph5_g: function() {}, ph5_h: [], ph5_j: {}, ph5_l: [123, 45.67, '123.456', 'Integer porttitor urna ultricies justo iaculis vulputate ac non neque. Etiam iaculis est quis urna consequat ornare. Suspendisse a augue sit amet urna tincidunt rhoncus sed non tellus. Mauris ultricies lectus sit amet tellus egestas, in interdum elit rhoncus. Nullam viverra lectus mattis felis semper, in vulputate lorem tincidunt. Suspendisse dapibus arcu id nunc blandit, ut egestas nisi pulvinar.', !0, !1, function() {}, { a: 321, b: 76.54, c: '654.321', d: '321 zyx' }] } } } } }; let c = []; for (let d = 0; d < a; d++)c.push(b); return c
                 })(20000), //200000 20000 1000
             },
