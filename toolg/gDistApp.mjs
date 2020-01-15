c3f049b168eea7e401089b4c26acb38e204c1f7e
yuda-lyu <firsemisphere@gmail.com>
2020-01-15 15:43:14 +0800

    modify: dist app by rollupVueToHtml

--- toolg/gDistApp.mjs
+++ toolg/gDistApp.mjs
@@ -1,15 +1,41 @@
-import rollupFiles from 'w-package-tools/src/rollupFiles.mjs'
+import rollupVueToHtml from 'w-package-tools/src/rollupVueToHtml.mjs'
 
 
-let fdSrc = './src'
-let fdTar = './docs/examples/'
+let opt = {
+    title: `w-component-vue`,
+    head: `
 
+    <!-- fontawesome -->
+    <link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
 
-rollupFiles({
-    fns: 'App.vue',
-    fdSrc,
-    fdTar,
-    nameDistType: 'kebabCase',
+    <!-- mdi, 各組件使用mdi/js故不需引用 -->
+    <link _href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
+
+    <!-- google, 各組件使用mdi/js故不需引用 -->
+    <link _href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link _href="https://fonts.googleapis.com/css?family=Material+Icons" rel="stylesheet">
+
+    <!-- data -->
+    <script src="https://cdn.jsdelivr.net/npm/w-demores/res/data/dataAAPL.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/w-demores/res/data/dataUSD2EUR.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/w-demores/res/data/dataTemperature.js"></script>
+    <script>
+        //save in window
+        window.dataAAPL=dataAAPL
+        window.dataUSD2EUR=dataUSD2EUR
+        window.dataTemperature=dataTemperature
+    </script>
+
+    <!-- element-ui, 已按需打包故不需引用 -->
+    <link _href="https://cdn.jsdelivr.net/npm/element-ui@2.13.0/lib/theme-chalk/index.css" rel="stylesheet">
+    <script _src="https://cdn.jsdelivr.net/npm/element-ui@2.13.0/lib/index.js"></script>
+
+    <!-- vuetify -->
+    <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.js"></script>
+
+    `,
+    newVue: `vuetify: new Vuetify(),`,
     globals: {
         'vue': 'Vue', //因動態加載組件, 現統一使用window內Vue, 故此處不打包進來, 若vue-cli則自動將Vue關聯至window內的Vue
         'vuetify': 'vuetify',
@@ -18,5 +44,6 @@ rollupFiles({
         'vue',
         'vuetify',
     ],
-})
+}
+rollupVueToHtml('./src/App.vue', './docs/examples/app.html', opt)
 
