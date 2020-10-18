73d6efacf0f46ceb870e4ff4f46bb415529d04ec
yuda-lyu <firsemisphere@gmail.com>
2020-10-18 16:31:04 +0800

    modify: update pathItems in WAggridVueDyn

--- src/components/WAggridVueDyn.vue
+++ src/components/WAggridVueDyn.vue
@@ -69,8 +69,8 @@ export default {
         pathItems: {
             type: Array,
             default: () => [
-                'https://cdn.jsdelivr.net/npm/ag-grid-community@23.2.1/dist/ag-grid-community.min.noStyle.js',
-                'https://cdn.jsdelivr.net/npm/ag-grid-vue@23.2.1/dist/ag-grid-vue.umd.min.js', //於es5內載入時, AgGridVue會出現在window['ag-grid-vue'].AgGridVue
+                'https://cdn.jsdelivr.net/npm/ag-grid-community@24.0.0/dist/ag-grid-community.min.noStyle.js',
+                'https://cdn.jsdelivr.net/npm/ag-grid-vue@24.0.0/dist/ag-grid-vue.umd.min.js', //於es5內載入時, AgGridVue會出現在window['ag-grid-vue'].AgGridVue
                 'https://cdn.jsdelivr.net/npm/w-aggrid-vue@2.0.0/dist/w-aggrid-vue.umd.js',
             ],
         },
