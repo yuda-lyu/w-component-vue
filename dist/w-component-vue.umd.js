d064b29be0ad1a47063ca66ad3627b15320fa1ff
yuda-lyu <firsemisphere@gmail.com>
2021-05-31 16:10:13 +0800

    modify: update pks

--- dist/w-component-vue.umd.js
+++ dist/w-component-vue.umd.js
@@ -1,5 +1,5 @@
 /*!
- * w-component-vue v2.0.52
+ * w-component-vue v2.0.53
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
--- docs/examples/app.umd.js
+++ docs/examples/app.umd.js
@@ -1,5 +1,5 @@
 /*!
- * app v2.0.52
+ * app v2.0.53
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
