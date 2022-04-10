aa28c7b29f2e9374c4707cc50f088dbff79bc684
yuda-lyu <firsemisphere@gmail.com>
2022-04-10 23:53:08 +0800

    modify: pathItems in WAggridVueDyn

--- src/components/WAggridVueDyn.vue
+++ src/components/WAggridVueDyn.vue
@@ -76,10 +76,10 @@ export default {
         pathItems: {
             type: Array,
             default: () => [
+                'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js', //w-aggrid-vue的download與upload會使用, 一併預載
                 'https://cdn.jsdelivr.net/npm/ag-grid-community@27.1.0/dist/ag-grid-community.min.noStyle.js',
                 'https://cdn.jsdelivr.net/npm/ag-grid-vue@27.1.0/dist/ag-grid-vue.umd.min.js', //於es5內載入時, AgGridVue會出現在window['ag-grid-vue'].AgGridVue
                 'https://cdn.jsdelivr.net/npm/w-aggrid-vue@2.0.28/dist/w-aggrid-vue.umd.js',
-                'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js', //download與upload會使用, 一併預載
             ],
         },
         opt: {
