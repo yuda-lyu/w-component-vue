91ede81e25d8b8d9583182ba2d4c92443932cefe
yuda-lyu <firsemisphere@gmail.com>
2022-05-30 22:40:35 +0800

    modify: update pathItems

--- src/components/WLeafletVueDyn.vue
+++ src/components/WLeafletVueDyn.vue
@@ -103,7 +103,7 @@ export default {
             type: Array,
             default: () => [
                 //現在w-leaflet-vue已依賴w-component-vue故為循環引用, 但使用dyn則為脫勾, 故仍需人工更新此處版本
-                'https://cdn.jsdelivr.net/npm/w-leaflet-vue@1.0.14/dist/w-leaflet-vue.umd.js',
+                'https://cdn.jsdelivr.net/npm/w-leaflet-vue@1.0.15/dist/w-leaflet-vue.umd.js',
             ],
         },
         opt: {
