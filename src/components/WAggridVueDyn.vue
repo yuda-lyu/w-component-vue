cdb92adc8e6bf6865dc7b7ac2851b0692d6c3097
yuda-lyu <firsemisphere@gmail.com>
2023-05-30 15:11:20 +0800

    modify: update pathItems

--- src/components/WAggridVueDyn.vue
+++ src/components/WAggridVueDyn.vue
@@ -77,9 +77,9 @@ export default {
             type: Array,
             default: () => [
                 'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js', //w-aggrid-vue的download與upload會使用, 一併預載
-                'https://cdn.jsdelivr.net/npm/ag-grid-community@27.1.0/dist/ag-grid-community.min.noStyle.js',
-                'https://cdn.jsdelivr.net/npm/ag-grid-vue@27.1.0/dist/ag-grid-vue.umd.min.js', //於es5內載入時, AgGridVue會出現在window['ag-grid-vue'].AgGridVue
-                'https://cdn.jsdelivr.net/npm/w-aggrid-vue@2.0.32/dist/w-aggrid-vue.umd.js',
+                'https://cdn.jsdelivr.net/npm/ag-grid-community@27.3.0/dist/ag-grid-community.min.noStyle.js',
+                'https://cdn.jsdelivr.net/npm/ag-grid-vue@27.3.0/dist/ag-grid-vue.umd.min.js', //於es5內載入時, AgGridVue會出現在window['ag-grid-vue'].AgGridVue
+                'https://cdn.jsdelivr.net/npm/w-aggrid-vue@2.0.33/dist/w-aggrid-vue.umd.js',
             ],
         },
         opt: {
