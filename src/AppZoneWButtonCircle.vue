209301f2628c52c629e60b0b4e11f327f4ce8333
yuda-lyu <firsemisphere@gmail.com>
2023-12-26 14:31:55 +0800

    modify: exam.

--- src/AppZoneWButtonCircle.vue
+++ src/AppZoneWButtonCircle.vue
@@ -180,7 +180,7 @@
                     :casename="'borderRadiusStyle(horizontal btns)'"
                 ></demolink>
 
-                <div style="display:inline-block; border-radius:8px; box-shadow:0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)">
+                <div style="display:inline-block; border-radius:8px; box-shadow:0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);">
                     <div style="display:flex;">
                         <w-button-circle
                             :icon="mdiCheckUnderlineCircle"
@@ -207,7 +207,7 @@
                     :casename="'borderRadiusStyle(vertical btns)'"
                 ></demolink>
 
-                <div style="display:inline-block; border-radius:8px; box-shadow:0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)">
+                <div style="display:inline-block; border-radius:8px; box-shadow:0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);">
                     <div style="display:flex; flex-direction:column;">
                         <w-button-circle
                             :icon="mdiCheckUnderlineCircle"
